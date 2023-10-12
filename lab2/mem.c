b2112d2e9e5ac55338ea188e2cac9d12fea522da
huttsMichael <huttsmichael@gmail.com>
2023-10-12 16:13:22 -0400

    Problem 1.2

--- lab2/mem.c
+++ lab2/mem.c
@@ -4,14 +4,32 @@
 
 static unsigned len = 16*(1 << 20);
 
+// Problem 1.1
+/*
 typedef struct Mem {
+	int fc;
+	int fe;
 	int fa;
 	char fb;
-	int fc;
 	char fd;
-	int fe;
 } Mem;
+*/
 
+// Problem 1.2
+typedef struct MemCE {
+	int fc;
+	int fe;
+} MemCE;
+
+typedef struct MemABD {
+	int fa;
+	char fb;
+	char fd;
+} MemABD;
+
+
+// Problem 1.1
+/*
 void func(Mem * a)
 {
 	int i;
@@ -25,6 +43,23 @@ void func(Mem * a)
 	}
 
 }
+*/
+
+
+// Problem 1.2
+void func(MemCE * ce, MemABD * abd)
+{
+	int i;
+
+	for(i=0; i<len; i++){
+		abd[i].fa = abd[i].fb+abd[i].fd;
+	}
+
+	for(i=0; i<len; i++){
+		ce[i].fc = ce[i].fe*2;
+	}
+
+}
 
 /* Please add your events here */
 int events[1] = {PAPI_L2_DCM}; /*PAPI_L1_DCM, PAPI_L2_DCM, PAPI_TLB_DM*/
@@ -34,7 +69,14 @@ int main()
 {
 	long long values[1];
 	int eventset;
+	// Problem 1.1
+	/*
 	Mem * a;
+	*/
+
+	// Problem 1.2
+	MemCE * ce;
+	MemABD * abd;
 
 	if(PAPI_VER_CURRENT != PAPI_library_init(PAPI_VER_CURRENT)){
 		printf("Can't initiate PAPI library!\n");
@@ -51,11 +93,31 @@ int main()
 		exit(-4);
 	}
 
+	// Problem 1.1
+	/*
 	a = (Mem *) malloc(len*sizeof(Mem));
+	*/
+
+	// Problem 1.2
+	ce = (MemCE *) malloc(len*sizeof(MemCE));
+	abd = (MemABD *) malloc(len*sizeof(MemABD));
 	PAPI_start(eventset);
+	// Problem 1.1
+	/*
 	func(a);
+	*/
+
+	// Problem 1.2
+	func(ce, abd);
 	PAPI_stop(eventset, values);
+	// Problem 1.1
+	/*
 	free(a);
+	*/
+
+	// Problem 1.2
+	free(ce);
+	free(abd);
 
 	/*Print out PAPI reading*/
 	char event_name[PAPI_MAX_STR_LEN];
