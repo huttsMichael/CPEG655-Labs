2f1da57a195488109a0e1074f9ac6fe87af2baf2
huttsMichael <huttsmichael@gmail.com>
2023-12-16 11:39:54 -0500

    reorder added

--- final/matrix_multiplication_sse_benchmark.cpp
+++ final/matrix_multiplication_sse_benchmark.cpp
@@ -36,6 +36,30 @@ void matrixMultiplySSE(float* A, float* B, float* C, int size) {
     }
 }
 
+// function to perform SSE-based matrix multiplication with JIK ordering
+void matrixMultiplySSEReorder(float* A, float* B, float* C, int size) {
+    // iterate over each column of matrix B 
+    for (int j = 0; j < size; j += size) {
+        // iterate over each row of matrix A
+        for (int i = 0; i < size; i++) {
+            __m128 rowA, vecB;
+            __m128 result = _mm_setzero_ps();
+            for (int k = 0; k < 4; k++) {
+                // load a single element from the current row of A and fill a vector
+                rowA = _mm_set1_ps(A[i * size + j + k]);
+                // load a vector from the current column of B
+                vecB = _mm_loadu_ps(B + k * size + j);
+
+                // multiply the rowA vector with the loaded vecB vector element-wise and add to the result
+                result = _mm_add_ps(result, _mm_mul_ps(rowA, vecB));
+            }
+
+            // store the result vector to the current position in matrix C
+            _mm_storeu_ps(C + i * size + j, result);
+        }
+    }
+}
+
 // function to perform SSE-based matrix multiplication (unrolled)
 void matrixMultiplySSEUnrolled(float* A, float* B, float* C, int size) {
     // iterate over each row of matrix A
@@ -70,6 +94,7 @@ void matrixMultiplySSEUnrolled(float* A, float* B, float* C, int size) {
     }
 }
 
+// function to perform SSE-based matrix multiplication (extremely unrolled)
 void matrixMultiplySSEUnrolledExtreme(float* A, float* B, float* C, int size) {
     // iterate over each row of matrix A
     for (int i = 0; i < size; i++) {
@@ -220,6 +245,7 @@ int main() {
     initializeRandomMatrix(B, size);
 
     float C_sse[size * size] = {0.0};
+    float C_sse_reorder[size * size] = {0.0};
     float C_sse_unrolled[size * size] = {0.0};
     float C_sse_unrolled_extreme[size * size] = {0.0};
     float C_non_sse[size * size] = {0.0};
@@ -232,6 +258,15 @@ int main() {
     clock_t stop_sse = clock();
     double duration_sse = static_cast<double>(stop_sse - start_sse) / CLOCKS_PER_SEC;
     double avg_duration_sse = duration_sse / num_repeats * 1e6; // convert to microseconds
+
+    // measure SSE-based matrix multiplication time
+    clock_t start_sse_reorder = clock();
+    for (int i = 0; i < num_repeats; ++i) {
+        matrixMultiplySSEReorder(A, B, C_sse_reorder, size);
+    }
+    clock_t stop_sse_reorder = clock();
+    double duration_sse_reorder = static_cast<double>(stop_sse_reorder - start_sse_reorder) / CLOCKS_PER_SEC;
+    double avg_duration_sse_reorder = duration_sse_reorder / num_repeats * 1e6; // convert to microseconds
 
     // measure SSE-based matrix multiplication with unrolled innermost loop time
     clock_t start_sse_unrolled = clock();
@@ -270,19 +305,23 @@ int main() {
 
     std::cout << "SSE-based Matrix Multiplication Result:\n";
     printMatrix(C_sse, size, size);
-    std::cout << "Average time taken by SSE-based matrix multiplication: " << avg_duration_sse << " microseconds\n\n";
+    std::cout << "Average time taken by SSE-based matrix multiplication: " << avg_duration_sse << " microseconds\n\n\n";
+
+    std::cout << "SSE-based Matrix Multiplication (Re-Ordered) Result:\n";
+    printMatrix(C_sse_reorder, size, size);
+    std::cout << "Average time taken by SSE-based matrix multiplication (Re-Ordered): " << avg_duration_sse << " microseconds\n\n\n";
 
     std::cout << "SSE-based Matrix Multiplication (Unrolled) Result:\n";
     printMatrix(C_sse_unrolled, size, size);
-    std::cout << "Average time taken by SSE-based matrix multiplication (Unrolled): " << avg_duration_sse_unrolled << " microseconds\n\n";
+    std::cout << "Average time taken by SSE-based matrix multiplication (Unrolled): " << avg_duration_sse_unrolled << " microseconds\n\n\n";
 
     std::cout << "SSE-based Matrix Multiplication (Unrolled Extreme) Result:\n";
     printMatrix(C_sse_unrolled_extreme, size, size);
-    std::cout << "Average time taken by SSE-based matrix multiplication (Unrolled Extreme): " << avg_duration_sse_unrolled_extreme << " microseconds\n\n";
+    std::cout << "Average time taken by SSE-based matrix multiplication (Unrolled Extreme): " << avg_duration_sse_unrolled_extreme << " microseconds\n\n\n";
 
     std::cout << "Non-SSE Matrix Multiplication Result:\n";
     printMatrix(C_non_sse, size, size);
-    std::cout << "Average time taken by non-SSE matrix multiplication: " << avg_duration_non_sse << " microseconds\n\n";
+    std::cout << "Average time taken by non-SSE matrix multiplication: " << avg_duration_non_sse << " microseconds\n\n\n";
 
     return 0;
 }
